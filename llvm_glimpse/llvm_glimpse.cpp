95db5a9b99a5eb2b3183e265e85eefeed5140fbb
Yyyyshen <Yyyyshen@gmail.com>
2021-03-03 14:27:19 +0800

    llvm了解

---
+++ llvm_glimpse/llvm_glimpse.cpp
@@ -0,0 +1,19 @@
+﻿// llvm_glimpse.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 了解编译原理
+ * 从llvm开始
+ * 
+ * 参考
+ * https://zhuanlan.zhihu.com/p/122522485
+ * https://llvm.org/docs/tutorial/index.html （官方）
+ */
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
