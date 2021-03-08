7f2d3efa3ab929eac55122146453e621237aa725
Yyyyshen <Yyyyshen@gmail.com>
2021-03-08 17:41:14 +0800

    随便写点啥

--- llvm_glimpse/llvm_glimpse.cpp
+++ llvm_glimpse/llvm_glimpse.cpp
@@ -10,6 +10,7 @@
  * 参考
  * https://zhuanlan.zhihu.com/p/122522485
  * https://llvm.org/docs/tutorial/index.html （官方）
+ * clang手册 https://clang.llvm.org/docs/UsersManual.html
  */
 
 int main()
@@ -17,3 +18,7 @@ int main()
     std::cout << "Hello World!\n";
 }
 
+/**
+ * ps:
+ * 2021/3/8 更新了vs 16.9.0，看日志好像很强，不过发现新版本在推送完项目到github会被拉黑一段时间，不知道是不是bug
+ */
