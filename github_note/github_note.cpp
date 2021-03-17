457d1253ad470fd3613a4bdd78b0c39279b365c8
Yyyyshen <Yyyyshen@gmail.com>
2021-03-17 14:09:32 +0800

    github上提issue时用到的代码折叠块

---
+++ github_note/github_note.cpp
@@ -0,0 +1,22 @@
+﻿// github_note.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/*
+之前在github给asio提issue的时候经常需要贴大段代码，很显然占据大块页面并不便于讨论问题
+所以查了一下折叠块的使用方法
+
+github 折叠块格式
+<details>
+<summary>123</summary>
+```java
+import(这里是代码区域)
+```
+</details>
+*/
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
