46f2fc44335ebb87ceb0ca6fd869520330bfbc4b
Yyyyshen <Yyyyshen@gmail.com>
2021-04-28 11:45:14 +0800

    随意看看chia coin

---
+++ chia_coin/chia_coin.cpp
@@ -0,0 +1,25 @@
+﻿// chia_coin.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+//chia币突然火了，随便研究下
+//
+//官网：
+// https://www.chia.net/
+// https://github.com/Chia-Network/chia-blockchain
+// 
+//一些参考文章：
+// https://sungkim11.medium.com/farm-chia-coin-xch-and-make-money-8fda188b8689 （教程和讲解，以及如何计算收益）
+// https://www.youtube.com/watch?v=yExAOpwTHHI （视频教程）
+// https://github.com/MrPig91/PSChiaPlotter （计算k-size的分配方案脚本）
+// https://plot-plan.chia.foxypool.io/ （在线计算k-size的分配方案）
+// https://www.hpool.com/help/tutorial/Chia%E6%8C%96%E7%9F%BF%E6%95%99%E7%A8%8B （加入其他矿池hpool教程）
+// https://chiacalculator.com/ （在线计算收益）
+// https://github.com/Chia-Network/chia-blockchain/wiki/FAQ （官方wiki-FAQ）
+//
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
